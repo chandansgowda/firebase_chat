2724ca7fb230ada086b1a9b5346825b1b53f663e
chandsgowda <chandansuresh007@gmail.com>
2022-06-08 09:43:36 +0530

    firebase integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:firebase_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
